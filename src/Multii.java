dae178433f3060099f815cacb2f1a6b77dcd5e24
adrian <adrian@DESKTOP-0IJQPBV>
2023-12-20 17:14:05 -0600

    segunda

--- src/Multii.java
+++ src/Multii.java
@@ -8,20 +8,22 @@ public class Multii {
             for(k=0;k<=8;k++){
             for(x=0;x<=2;x++){
                 mut1[v][b]=Short.parseShort(JOptionPane.showInputDialog("inserta numeros matriz 1"));
-            
+           
              
              for(y=0;y<=2;y++){
                  mut2[m][n]=Short.parseShort(JOptionPane.showInputDialog("inserta numeros matriz 2 "));
              multi= (byte) (mut1[v][b]*mut2[m][n]);
                 
+               System.out.print( multi);
                
              }
-             System.out.print( multi+"\t");
-             System.out.println();
+             
+             System.out.print("\t");
             }
+            System.out.println();
             
             }
-          
+           
             
            
                 
